6e8249613401c8aab7f28c2fdb5dcad83f8d0405
Luigi Minervini <minervini.luigi1@gmail.com>
2022-09-06 00:36:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
+app.get("/with-svc", (req, res) => {
   axios
     .get("http://demo-be.gigium-dev.svc.cluster.local:8080/")
     .then((resp) => {
@@ -20,6 +20,22 @@ app.get("/", (req, res) => {
     .catch((e) => console.log(e));
 });
 
+
+app.get("/with-route", (req, res) => {
+  axios
+    .get("https://demo-be-gigium-dev.apps.sandbox.x8i5.p1.openshiftapps.com")
+    .then((resp) => {
+      console.log(resp.data.color);
+      res.send(
+        '<html > <head><title>DEMO FE</title></head><body bgcolor="' +
+          resp.data.color +
+          '"><h1>' +
+          resp.data.color +
+          "</h1></body> </html>"
+      );
+    })
+    .catch((e) => console.log(e));
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
