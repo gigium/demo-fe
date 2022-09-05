5576d7e56555cb21db81ce1d5cb3164bbf346edd
Luigi Minervini <minervini.luigi1@gmail.com>
2022-09-06 00:27:11 +0200

    deploy node app

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  axios
+    .get("https://demo-be-gigium-dev.apps.sandbox.x8i5.p1.openshiftapps.com/")
+    .then((resp) => {
+      console.log(resp.data.color);
+      res.send(
+        '<html > <head><title>DEMO FE</title></head><body bgcolor="' +
+          resp.data.color +
+          '"><h1>' +
+          resp.data.color +
+          "</h1></body> </html>"
+      );
+    })
+    .catch((e) => console.log(e));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
